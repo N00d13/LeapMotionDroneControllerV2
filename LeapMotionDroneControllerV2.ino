f2dbd096df24d130ad07023245da1800e501c7ce
Matthew <mpkaye2@gmail.com>
2022-07-15 15:00:08 -0700

    Created documentation for Arduino file

--- LeapMotionDroneControllerV2.ino
+++ LeapMotionDroneControllerV2.ino
@@ -1,24 +1,24 @@
-const uint8_t NO_OF_PPM_CHANNELS = 6;  //Number of channels being used
-const uint16_t DEFAULT_SERVO_VALUE = 1500;  //Default channel value
-const uint16_t MIN_SERVO_VALUE = 1150;  //Minimum servo value
-const uint16_t MAX_SERVO_VALUE = 1850;  //Minimum servo value
-const uint16_t PPM_FRAME_LEN = 22500;  //PPM frame length in microseconds
-const uint16_t PPM_PULSE_LEN = 300;  //Pulse length in microseconds
-const uint16_t IS_POSITIVE_POLARITY = 0;  //Polarity of the pulses (0 negative and 1 positive)
-const uint8_t SIGNAL_PIN = 10;  //Output pin on arduino (use 9 or 9 to use Timer1 for PPM)
+const uint8_t NO_OF_PPM_CHANNELS = 6;  //Number of channels being used.
+const uint16_t DEFAULT_SERVO_VALUE = 1500;  //Default channel value.
+const uint16_t MIN_SERVO_VALUE = 1150;  //Minimum servo value.
+const uint16_t MAX_SERVO_VALUE = 1850;  //Minimum servo value.
+const uint16_t PPM_FRAME_LEN = 22500;  //PPM frame length in microseconds.
+const uint16_t PPM_PULSE_LEN = 300;  //Pulse length in microseconds.
+const uint16_t IS_POSITIVE_POLARITY = 0;  //Polarity of the pulses (0 negative and 1 positive).
+const uint8_t SIGNAL_PIN = 10;  //Output pin on arduino (use 9 or 9 to use Timer1 for PPM).
 
-volatile uint16_t Ppm[NO_OF_PPM_CHANNELS]; //Array that holds servo values for ppm signal (usually ranges from 1000-2000)
-volatile uint16_t PpmIn[NO_OF_PPM_CHANNELS]; //Array that holds servo values for ppm signal after read (usually ranges from 1000-2000)
+volatile uint16_t Ppm[NO_OF_PPM_CHANNELS]; //Array that holds servo values for ppm signal (usually ranges from 1000-2000).
+volatile uint16_t PpmIn[NO_OF_PPM_CHANNELS]; //Array that holds servo values for ppm signal after read (usually ranges from 1000-2000).
 
 /*
-   Setup for ppm array values and Serial
+   Program that sends PPM from information read through the serial COM port.
 */
 
+// MODIFIES: this.
+// EFFECT: Initializes the program waits for the serial COM port to be connected.
 void setup()
 {
   Serial.begin(38400); //Begins serial
-  //Serial.println("SERIAL OPENED");
-
 
   //Initialize all channels with default value
   for (uint8_t channel = 0; channel < sizeof(PpmIn) / sizeof(int); channel++)
@@ -27,37 +27,29 @@ void setup()
   }
 
   /* SETUP PPM */
-  pinMode(SIGNAL_PIN, OUTPUT);
+  pinMode(SIGNAL_PIN, OUTPUT); // Sets pin behavior as ouput
   digitalWrite(SIGNAL_PIN, !IS_POSITIVE_POLARITY);  //set the PPM signal pin to the default state (off)
 
   cli();
-  TCCR1A = 0; // set entire TCCR1 register to 0
-  TCCR1B = 0;
+  TCCR1A = 0; // Set entire TCCR1 register to 0
+  TCCR1B = 0; // Set entire TCCR1 register to 0
 
-  OCR1A = 100;  // compare match register, change this
-  TCCR1B |= (1 << WGM12);  // turn on CTC mode
+  OCR1A = 100;  // Compare match register, change this
+  TCCR1B |= (1 << WGM12);  // Turn on CTC mode
   TCCR1B |= (1 << CS11);  // 8 prescaler: 0,5 microseconds at 16mhz
-  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A); // Enable timer compare interrupt
   sei();
 
-  /* SETUP SERIAL */
-  //Waits until Serial Port initialized
   while (!Serial) {
-    //stub
+    //Waits until Serial Port initialized
   }
-
-  //Serial.println("SERIAL STARTED");
-
 }
 
-/*
-  Main loop
 
-  - takes information for ppm and channel values
-  - Updates PPM information
-  - Send information through PPM frame
-*/
-//test
+// MODIFIES: this.
+// EFFECT: Read the incoming information from the serial COM port.
+//         Sets the corresponding ppm channel numbers.
+//         Sends the channel numbers into single frame to the transmitter.
 void loop() {
   String incomingData = "";
   if (Serial.available()) {
@@ -82,17 +74,16 @@ void loop() {
 
 }
 
-
+// MODIFIES: this.
+// EFFECT: Sets the corresponding channels to the correct PPM values.
 void setPPM(uint8_t channel_num, uint16_t ppm_value) {
   PpmIn[channel_num - 1] = ppm_value;
 }
 
-/*
-  Creates PPM signal
-*/
+// MODIFIES: this.
+// EFFECT: Sends the channel information into PPM signals to the transmitter.
 ISR(TIMER1_COMPA_vect)
 {
-
   static boolean state = true;
 
   //copy incomming values to ppm array
