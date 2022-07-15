dbecf8c1f92293507396025eb92261400107d5a4
Matthew <mpkaye2@gmail.com>
2022-07-15 12:58:07 -0700

    Created Documentation for LeapMotionTX

--- src/main/model/LeapMotionTX.java
+++ src/main/model/LeapMotionTX.java
@@ -3,17 +3,20 @@ package main.model;
 import com.leapmotion.leap.Hand;
 import com.leapmotion.leap.HandList;
 
+// Calculates the normal vector of the left and right hand and returns the angle corresponding to the required channels (thrust, pitch, roll, yaw).
 public class LeapMotionTX {
-    private Hand leftHand;
-    private Hand rightHand;
+    private Hand leftHand; //Hand object representing the left hand
+    private Hand rightHand; // Hand object representing the right hand
 
+    // MODIFIES: this
+    // EFFECT: Assigns the left and right-hand values to null.
     public LeapMotionTX(){
-        leftHand = null;
         leftHand = null;
         rightHand= null;
     };
 
-    // Assigns the left and right hand on each frame to its respective variable
+    // MODIFIES: this
+    // EFFECT: Assigns the left and right hand on each frame to its respective variable.
     public void assignHands(HandList hands) {
         if (hands.get(0).isRight()) {
             this.rightHand = hands.get(0);
@@ -28,7 +31,7 @@ public class LeapMotionTX {
         }
     }
 
-    // Returns true if both hands are available, otherwise returns false
+    // EFFECT: Returns true if both hands are available, otherwise returns false.
     public boolean HandsAvailable() {
 
         if ((leftHand != null && rightHand != null) && (leftHand.isValid() && rightHand.isValid())) { //
@@ -37,19 +40,15 @@ public class LeapMotionTX {
         return false;
     }
 
-    // Returns the roll degrees between -90 and 90 for the right hand
-    // Rolling right is positive and left is negative
-    // Returns -1 if right hand isn't available
+    // EFFECT: Returns the roll degrees between -90 and 90 for the right hand.
+    //         Rolling right is positive and left is negative.
+    //         Returns -1 if right hand isn't available.
     public float getRightRollDegrees() {
         if (rightHand.isRight()) {
             float rollVector = rightHand.palmNormal().roll();
 
-            //System.out.println(rollVector);
-
             float rollDegrees = rollVector * (float) (180/Math.PI);
             rollDegrees *= -1;
-
-            //System.out.println( rollDegrees);
 
             if (rollDegrees < -45 || (rollDegrees > 100 && rollDegrees < 150)) {
                 rollDegrees = -45;
@@ -57,25 +56,20 @@ public class LeapMotionTX {
                 rollDegrees = 45;
             }
 
-            System.out.println( rollDegrees);
-
             return (rollDegrees);
         }
         return -1;
     }
 
-    // Returns the pitch degrees between -90 and 90 for right hand
-    // A flat hand is 0 degrees
-    // Pitching up give a positive pitch up to 90
-    // Pitch down gives a negative pitch up to -90
-    // Returns -1 if right hand isn't available
+    // EFFECT: Returns the pitch degrees between -90 and 90 for right hand.
+    //         A flat hand is 0 degrees.
+    //         Pitching up give a positive pitch up to 90 and pitch down gives a negative pitch up to -90.
+    //         Returns -1 if right hand isn't available.
     public float getRightPitchDegrees() {
         if (rightHand.isRight()) {
             float pitchVector = rightHand.palmNormal().pitch();
             float pitchDegrees = pitchVector * (float) (180/Math.PI);
             pitchDegrees = pitchConversion(pitchDegrees);
-
-            //System.out.println("Pitch Degrees: " + pitchDegrees);
 
             if (pitchDegrees < -45) {
                 pitchDegrees = -45;
@@ -88,10 +82,10 @@ public class LeapMotionTX {
         return -1;
     }
 
-    // Returns the yaw degrees for right hand
-    // Hand facing forward is 0 degrees
-    // Yaw right gives value between 0 to 180 (drone faces right)
-    // Yaw facing left gives value between -180 to 0
+    // EFFECT: Returns the yaw degrees for the left hand.
+    //         Hand facing forward is 0 degrees.
+    //         Yaw right gives value between 0 to 180 (drone faces right).
+    //         Yaw facing left gives value between -180 to 0.
     public float getLeftYawDegrees(){
         if (leftHand.isLeft()) {
             float yawVector = leftHand.palmNormal().roll();
@@ -110,11 +104,11 @@ public class LeapMotionTX {
         return -1;
     }
 
-    // Returns the pitch degrees between 0 and 90 for left hand
-    // A flat hand is 0 degrees
-    // Pitching up give a positive pitch up to 90
-    // Pitch down gives a negative pitch up to 0
-    // Returns -1 if left hand isn't available
+    // EFFECT: Returns the thrust degrees between 0 and 90 for left hand.
+    //         A flat hand is 0 degrees.
+    //         Pitching up give a positive thrust up to 90.
+    //         Pitch down gives a lower bound thrust of 0.
+    //         Returns -1 if left hand isn't available.
     public float getLeftThrustingPitch() {
         if (leftHand.isLeft()) {
             float pitchVector = leftHand.palmNormal().pitch();
@@ -133,21 +127,7 @@ public class LeapMotionTX {
         return -1;
     }
 
-    // Returns false if drone shouldn't be armed and true if it should be
-    // If the left hand isn't over the Leap Motion or isn't flat within += 40 degrees then returns false
-    // If the left hand is within += 40 degrees of being flat then returns true
-    public boolean getLeftRollArming() {
-        if (leftHand.isLeft()) {
-            float rollVector = leftHand.palmNormal().roll();
-            float rollDegrees = rollVector * (float) (180/Math.PI);
-            if (rollDegrees > -40 && rollDegrees < 40) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    //Converts the pitch to rotate the degrees by 90
+    // EFFECT: Converts the pitch to rotate the degrees by 90 for conversion to Arduino.
     public float pitchConversion(float pitchDegrees) {
         if (pitchDegrees + 90 > 180) {
             pitchDegrees += (90 - 180);
@@ -158,6 +138,8 @@ public class LeapMotionTX {
         }
     }
 
+    // EFFECT: Returns a shifted channel level for conversion to Arduino.
+    //         If the channel name inputted doesn't exist then returns -1.
     public int ChannelShift(float value, String channel){
         //channel shift for right roll
         if (channel == "RR"){
